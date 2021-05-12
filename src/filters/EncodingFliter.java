0c811a216bf41dcf50787a8f48fce826addbb53e
akihiro8181 <akihirohataecc@gmail.com>
2021-05-12 15:29:11 +0900

    Add TopPage and EncodingFilter

---
+++ src/filters/EncodingFliter.java
@@ -0,0 +1,48 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFliter
+ */
+@WebFilter("/*")
+public class EncodingFliter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public EncodingFliter() {
+
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
